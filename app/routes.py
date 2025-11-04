0cb6e2441d3aad43c912f46cc93e3f45972e5d1f
Oscar-hub330 <madalaneoscar50@gmail.com>
2025-11-04 21:41:27 +0200

    Added Flask setup and updated project files

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, render_template, current_app
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
-def hello_world():
-    return jsonify({'message': 'Welcome to JESS API!'})
+def index():
+    return render_template('login.html', 
+                         config={
+                             'SUPABASE_URL': current_app.config['SUPABASE_URL'],
+                             'SUPABASE_KEY': current_app.config['SUPABASE_KEY']
+                         })
+
+@main.route('/dashboard')
+def dashboard():
+    return "Dashboard page - to be implemented"
--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
-from decouple import config
+from decouple import config # type: ignore
 
 class Config:
     SUPABASE_URL = config('https://hdzdxwssctktnlxsalws.supabase.co')
-    SUPABASE_KEY = config('Msimangona@123')
-    SUPABASE_SERVICE_KEY = config('5iTH1Wna56EhgGwYTodXDKi8e6phZ46MFvPy/S+KvnyF2WwILei8KCqaRbPS1yuYtRLUJETUrCLz/SSrz+l3Yg==')
-    SECRET_KEY = config('FLASK_SECRET_KEY')
+    SUPABASE_KEY = config('process.env.SUPABASE_KEY')
+    SUPABASE_SERVICE_KEY = config('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkemR4d3NzY3RrdG5seHNhbHdzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MTc0MTk4MSwiZXhwIjoyMDc3MzE3OTgxfQ.uperta_ChdHlnNfrHLNdZ83LWpiuStDpxoKnwe0U0ec')
+    SECRET_KEY = config('Msimangona@123')
